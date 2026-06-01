cc9e82eff12e41216803fa65f512f0f2864f5944
Sinchai <steeched@gmail.com>
2026-06-01 07:09:03 +0700

    Add force-dynamic to bypass build SSG

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link"; 
 import prisma from "../lib/prisma";
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   // ดึงข้อมูลโปรโมชั่น
